34a8f662b9d4a85dcc610268bfcaa2d9c29cf9c7
Jose Leandro Santos Justin <joselsjustin@gmail.com>
2023-03-16 13:31:44 -0300

    chore: install and configure nodemon

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const { app } = require('./app');
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on ${port}`);
 });
