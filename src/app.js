c84861e4d6bda4cc16fbc860a3a357c5a075f2b8
Jose Leandro Santos Justin <joselsjustin@gmail.com>
2023-03-17 07:54:56 -0300

    chore: add helmet to improve security

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(express.json());
-app.get('/test', (_, res) => {
+app.use(helmet());
+app.get('/test', (req, res) => {
   res.status(200).json({ name: 'jose' });
 });
 
